5b254e31765e152c4e935117cf845c76f25d53ac
rajeshwalker <98679393+rajeshwalker@users.noreply.github.com>
2022-07-13 20:11:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('roll-btn').addEventListener('click',rollDice)
 function rollDice(){
     var random = Math.floor(Math.random() * (7 - 1)) + 1
 
-    var source = './images/dice'+random+'.png'
+    var source = 'dice'+random+'.png'
 
     document.getElementById('image').setAttribute('src',source)
 
