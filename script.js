eb3ac491150549c65ac4d7700e50676e0403108f
rajeshwalker <98679393+rajeshwalker@users.noreply.github.com>
2022-07-13 20:04:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('roll-btn').addEventListener('click',rollDice)
+
+function rollDice(){
+    var random = Math.floor(Math.random() * (7 - 1)) + 1
+
+    var source = './images/dice'+random+'.png'
+
+    document.getElementById('image').setAttribute('src',source)
+
+    document.getElementById('result').innerHTML = 'You Got : '+random
+}
